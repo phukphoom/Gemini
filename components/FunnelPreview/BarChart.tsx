b47e594d915b74428e92652121370a4581e5415d
Phukphoom Taphrae <phoom0529@gmail.com>
2022-11-20 01:33:46 +0700

    fix: incorrect display chart

--- components/FunnelPreview/BarChart.tsx
+++ components/FunnelPreview/BarChart.tsx
@@ -23,40 +23,46 @@ const BarChart: FunctionComponent<BarChartProps> = ({
         type: "category",
         data: eventNameData,
       },
-      yAxis: {
-        min: 0,
-        max: () =>
-          Math.ceil((100 * Math.max(...eventScoreData)) / eventScoreData[0]),
-        type: "value",
-        axisLabel: {
-          formatter: (value: any) => {
-            return value + "%";
-          },
-        },
-        splitNumber: 10,
-        minorTick: {
-          show: true,
-        },
-      },
-      series: [
+      yAxis: [
         {
-          data: eventScoreData,
-          type: "bar",
-          label: {
+          min: 0,
+          max: 100,
+          type: "value",
+          axisLabel: {
+            formatter: (value: any) => {
+              return value + "%";
+            },
+          },
+          splitNumber: 10,
+          minorTick: {
             show: true,
           },
+        },
+      ],
+      series: [
+        {
+          data: eventScoreData.map((data) =>
+            Math.round((data / Math.max(...eventScoreData)) * 100)
+          ),
+          type: "bar",
           stack: "x",
           color: "#0A62B0",
+          label: {
+            show: true,
+            formatter: (data: any) => {
+              return eventScoreData[data.dataIndex];
+            },
+          },
         },
         {
           data: eventDiffs,
           type: "bar",
+          stack: "x",
+          color: "rgba(69, 129, 183, 0.3)",
           label: {
             show: true,
             color: "#0A62B0",
           },
-          stack: "x",
-          color: "rgba(69, 129, 183, 0.3)",
         },
       ],
     };
