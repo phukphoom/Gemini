c948d3f0ad99bef251dd8882538ddd2d1de31695
Phukphoom Taphrae <phoom0529@gmail.com>
2022-11-19 12:39:36 +0700

    feat: config tailwind.config

--- components/FunnelEditor/index.tsx
+++ components/FunnelEditor/index.tsx
@@ -1,7 +1,29 @@
 import { FunctionComponent } from "react";
+import Image from "next/image";
 
 const FunnelEditor: FunctionComponent = () => {
-  return <div className="bg-red-500 w-full h-full"></div>;
+  return (
+    <div className="w-full h-screen overflow-auto">
+      <div className="flex flex-col space-y-6 px-16 py-8">
+        <div className="flex flex-row justify-start items-center space-x-2">
+          <Image src="/favicon.ico" alt="Gemini" width={40} height={40} />
+          <p className="font-black font-garamond">Gemini</p>
+        </div>
+        <div className="flex flex-col space-y-2">
+          <p className="text-gemini-600 text-2xl">
+            Lets' create a scenario from eventInfos!
+          </p>
+          <p>
+            EventInfos are actions or methods defined in smart contracts of a
+            particular protocol.{"  "}
+            <a className="text-gemini-500 font-bold" href="">
+              Learn more
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FunnelEditor;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,41 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        futura: ["Futura Std", "sans-serif"],
+        garamond: ["Garamond", "sans-serif"],
+      },
+      fontWeight: {
+        bold: "650",
+      },
+      colors: {
+        gemini: {
+          200: "#D6EDF0",
+          300: "#B3DBE0",
+          400: "#4581B7",
+          500: "#0A62B0",
+          600: "#255682",
+        },
+        grey: {
+          200: "#E4E4E4",
+          300: "#CCCCCC",
+          400: "#C8C8C8",
+          500: "#828282",
+          600: "#00303C",
+          700: "#011324",
+        },
+        error: {
+          200: "#FCC9C8",
+          300: "#F39190",
+          400: "#E8605E",
+          500: "#EF4C49",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
