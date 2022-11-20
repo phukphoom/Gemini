82e75b24b8f4c8641b257759afc01d6c9949db47
Phukphoom Taphrae <phoom0529@gmail.com>
2022-11-20 17:22:11 +0700

    chore: last commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
     setLoading(true);
     const res = await axios({
       method: "post",
-      url: "http://154.215.14.233:3000/event",
+      url: "http://indexer.celab.network/event",
       data: { events: eventDetails, start: startTimeStamp, end: endTimeStamp },
     });
     setLoading(false);
