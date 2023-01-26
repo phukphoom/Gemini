62897b0f5cdb344eede38152fd48d7cb556891ae
TNattawutK <tomkabtokom@gmail.com>
2023-01-27 01:13:30 +0700

    feat: add provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material";
 import { StyledEngineProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { Provider } from "react-redux";
+
+import store from "../state";
 
 import "../styles/index.css";
 import "../styles/MatUIOverride.css";
@@ -14,12 +17,14 @@ const MatUITheme = createTheme({
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={MatUITheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <Provider store={store}>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={MatUITheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </Provider>
   );
 };
 
