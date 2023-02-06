825f4bf99a145f0abcf32783c2f8b5e33e58b5b6
TNattawutK <tomkabtokom@gmail.com>
2023-02-06 19:53:42 +0700

    feat: add icon

---
+++ components/Shared/Icons/IconClose.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from "react";
+
+import { IconProps } from "./types";
+
+const IconClose: FunctionComponent<IconProps> = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+};
+
+export default IconClose;
--- components/Shared/Icons/index.ts
+++ components/Shared/Icons/index.ts
@@ -1,4 +1,7 @@
 export { default as IconChevronLeft } from "./IconChevronLeft";
 export { default as IconChevronRight } from "./IconChevronRight";
-export { default as IconPlus } from "./IconPlus";
+export { default as IconClose } from "./IconClose";
 export { default as IconClock } from "./IconClock";
+export { default as IconEllipsisVertical } from "./IconEllipsisVertical";
+export { default as IconPlus } from "./IconPlus";
+export { default as IconTrash } from "./IconTrash";
